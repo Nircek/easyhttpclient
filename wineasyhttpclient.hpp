4d9ed05daddfb751699ef2e3a61a20c4d0e2d07f
Nircek <marcinek0302@gmail.com>
2017-04-04 22:11:56 +0200

    Some corrections

--- wineasyhttpclient.hpp
+++ wineasyhttpclient.hpp
@@ -4,13 +4,9 @@
 //ADD wininet TO LINKER
 #include <sstream>
 /*/Please remove the star on left but dont remove namespaces*/namespace GitHub{namespace Nircek{
-
 //  /*/comment*/something();
 //  change to
 //  //comment*/something();
-
 std::string wineasyhttpclient(std::string url);
 /*/*/}}
-
-
 #endif // WINEASYHTTPCLIENT_HPP_INCLUDED

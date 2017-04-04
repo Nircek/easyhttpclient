fd0cabfd71c6ec9053d2c0eff029583913beb311
Nircek <marcinek0302@gmail.com>
2017-04-04 22:12:36 +0200

    ADD README

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "wineasyhttpclient.hpp"
-using namespace std;
-using namespace GitHub::Nircek;
-int main()
-{
-    cout<<wineasyhttpclient("https://github.com/Nircek/")<<'\n'<<'\n';
-    return 0;
-}
